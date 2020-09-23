374bacd79dd40ab09aeb14e685fc60699fea8b85
Yuliia Lohvinova <yuliia.lohvinova@computools.com>
2020-09-23 14:48:28 +0300

    first commit

---
+++ precache-manifest.34c4a9d1e337bd6dbd00091e4d3e7995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d35b022ad8238fe509e6e0a7e07c9c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5894f2249714221f1dd",
+    "url": "/static/css/main.1fcb067e.chunk.css"
+  },
+  {
+    "revision": "ed11419cf97acc68fb3b",
+    "url": "/static/js/2.3cc9a166.chunk.js"
+  },
+  {
+    "revision": "edfd5a8be4ec1fc32e761a9220464431",
+    "url": "/static/js/2.3cc9a166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5894f2249714221f1dd",
+    "url": "/static/js/main.77f40a98.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f073f44ba639b3f45519878b8e0b4c3c",
+    "url": "/static/media/cocktail.f073f44b.png"
+  }
+]);
